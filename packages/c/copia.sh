519f031d1398eac6e6a381ecbc1720ec3ac3b070
vcatafesta <vcatafesta@gmail.com>
2020-08-07 11:51:06 -0400

    upgrade

--- packages/c/copia.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source /etc/bashrc
-
-for letra in {a..z}
-do
-	log_info_msg "Copiando arquivos iniciados com a letra: $letra para diretorio /github/ChiliOS/packages/$letra/"
-	cp $letra* /github/ChiliOS/packages/$letra/ &>/dev/null
-	evaluate_retval
-done
